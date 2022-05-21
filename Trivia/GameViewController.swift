5f349370d828a73b1dc63bdab293b1f0aaf36174
Lucia Heredia <luciaheredia22@gmail.com>
2022-05-21 14:42:42 +0300

    new screen

---
+++ Trivia/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  Trivia
+//
+//  Created by lucia heredia on 21/05/2022.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "game") as! GameViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
 
 
 }
